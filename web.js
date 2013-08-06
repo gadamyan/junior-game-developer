c66a5789b73537e00c5fa02acf61847f0b7de3aa
Gevorg Adamyan <gevorg.ad@gmail.com>
2013-08-06 05:02:51 +0400

    Added README.md.

--- web.js
+++ web.js
@@ -1,17 +1,3 @@
-/*var express = require("express");
-var app = express();
-app.use(express.logger());
-
-app.get('/', function(request, response) {
-    response.send('Hello World!');
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-    console.log("Listening on " + port);
-})
-*/
-
 /**
  * Module dependencies.
  */
@@ -19,7 +5,6 @@ app.listen(port, function() {
 var express = require('express');
 var routes = require('./routes');
 var user = require('./routes/user');
-var http = require('http');
 var ejs = require('ejs');
 var path = require('path');
 
@@ -52,9 +37,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-//http.createServer(app).listen(app.get('port'), function(){
-//  console.log('Express server listening on port ' + app.get('port'));
-//});
 app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
 });

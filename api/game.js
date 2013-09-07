ea4f94b6a5d912f3594dae8662a61f5b12dd9bc0
Gevorg Adamyan <gevorg.ad@gmail.com>
2013-09-07 19:05:16 +0400

    Added listing functionality

--- api/game.js
+++ api/game.js
@@ -1,10 +1,22 @@
 exports.list = function(req, res) {
-  res.json([{
-    name: 'Warzone 2100',
-    imageUrl: 'http://main.makeuseoflimited.netdna-cdn.com/wp-content/uploads/2010/10/wz2100.jpe',
-    author: 'some author',
-    user: 'name'
-  }]);
+  global.db.game.findAll().success(function(games) {
+    var games_json = [];
+    games.forEach(function(game) {
+      games_json.push({
+        name:         game.name,
+        author:       game.author,
+        imageUrl:     game.imageUrl,
+        downloadLink: game.downloadLink,
+        platform:     game.platform,
+        description:  game.description,
+        //dateCreation: req.body.dateCreation
+      });
+    });
+    res.send(games_json);
+  }).error(function(err) {
+    console.log(err);
+    response.send(err);
+  });
 };
 
 exports.add = function(req, res) {
